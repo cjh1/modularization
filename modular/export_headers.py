4af344bd2f95ee1810d75d42da04ceeba883c64e
Chris Harris <chris.harris@kitware.com>
2012-02-08 11:33:00 -0500

    ENH: Modified export_headers.py to skip headers that don't need export include
    Also removed exclude list for modules, the above functionality gets
    around this problem.
    
    Change-Id: Ife6dd03f3207a53ef9346a54974f73d0c3e039d5

--- modular/export_headers.py
+++ modular/export_headers.py
@@ -10,33 +10,45 @@ replaceText = "VTKCOMMONCORE_EXPORT"
 
 def searchReplace(path, search, replace, headerName):
   files = glob.glob(path + "/*.h*")
-  includeLine = "#include \"" + headerName + "\" // For export macro\n"
   if files is not []:
     for file in files:
       if os.path.isfile(file) and file != "Common/Core/vtkWin32Header.h" and file != "Common/Core/vtkSystemIncludes.h" and file != "Common/Core/vtkABI.h":
         firstInclude = True
-        for line in fileinput.input(file, inplace=1):
-          lineNumber = -1
+        lines = []
+        exportUpdated = False
+        with open(file, 'r') as fp:
+            lines = fp.readlines()
+
+        for index, line in enumerate(lines):
           lineNumber = pattern.search(line)
           if lineNumber >= 0 and line.find("VTK_INFORMATION_EXPORT") < 0:
             #print "Found a line to replace: " + line
             line = pattern.sub(replace, line)
-            line = line.rstrip() + "\n"
-          lineNumber = -1
-          lineNumber = string.find(line, "#include")
-          if lineNumber >= 0 and firstInclude == True:
-            line = includeLine + line
-            firstInclude = False
-          sys.stdout.write(line)
+            lines[index] = line.rstrip() + "\n"
+            exportUpdated = True
 
-excludes = ['Utilities/ParseOGLExt']
+        if exportUpdated:
+          includeLine = "#include \"" + headerName + "\" // For export macro\n"
+          addExportHeaderInclude(lines, includeLine)
+          with open(file, 'w') as fp:
+            for line in lines:
+              fp.write(line)
+
+def addExportHeaderInclude(lines, includeLine):
+  for index, line in enumerate(lines):
+    pos = string.find(line, "#include")
+
+    if pos >= 0:
+      lines[index] = includeLine + line
+      break
+
+  return lines
 
 def getModules():
   modules = []
   for root, dirnames, filenames in os.walk('.'):
     for filename in fnmatch.filter(filenames, 'module.cmake'):
-      if not root[2:] in excludes:
-        modules.append(root[2:])
+      modules.append(root[2:])
   return modules
 
 modules = getModules()
