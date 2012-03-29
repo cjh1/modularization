57fbb31a883d25eb06cd053498127c112f764425
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2012-03-29 14:00:30 -0400

    Moving stuff into modular configured headers.
    We should not have anything module/configuration specific in the
    vtkConfigure.h (just started here), and everything in vtkToolkit.h
    should be migrated to the module headers eventually and this file
    entirely removed.

---
+++ modular/GUISupport/Qt/module.cmake
@@ -0,0 +1,9 @@
+vtk_module(vtkGUISupportQt
+  DEPENDS
+    vtkCommonCore
+    vtkCommonDataModel
+    vtkCommonExecutionModel
+    vtkCommonMath
+    vtkRenderingCore
+    vtkImagingCore
+  DEFAULT ON)
