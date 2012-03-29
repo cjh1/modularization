9bbe8a765e30ec4c3e93be7cd0ae42272b41df5a
Nikhil Shetty <nikhil.shetty@kitware.com>
2012-03-29 16:50:06 -0400

    Bringing back code which was changed by mistake

--- modular/Infovis/Parallel/module.cmake
+++ modular/Infovis/Parallel/module.cmake
@@ -1,7 +1,13 @@
+if(VTK_USE_MPI)
+  set(_EXCLUDE_FROM_ALL)
+else()
+  set(_EXCLUDE_FROM_ALL EXCLUDE_FROM_ALL)
+endif()
 vtk_module(vtkInfovisParallel
   DEPENDS
-    vtkInfovisCore
-    vtkCommonExecutionModel
-    vtkParallelMPI
-    vtkFiltersParallel
+  vtkInfovisCore
+  vtkCommonExecutionModel
+  vtkParallelMPI
+  vtkFiltersParallel
+  ${_EXCLUDE_FROM_ALL}
   DEFAULT OFF)
