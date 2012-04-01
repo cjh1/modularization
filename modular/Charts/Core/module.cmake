1814d5ac46b96ad8a601d715860729182b624fbc
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2012-04-01 19:44:31 -0400

    Removed spurious dependency on ChartsCore.
    Only needed by the tests, for includes that are not used.

--- modular/Charts/Core/module.cmake
+++ modular/Charts/Core/module.cmake
@@ -2,7 +2,6 @@ vtk_module(vtkChartsCore
   DEPENDS
     vtkRenderingContext2D
     vtkInfovisCore # Needed for plot parallel coordinates vtkStringToCategory
-    vtkViewsInfovis # Needed for vtkRenderView
   TEST_DEPENDS
     vtkTestingCore
     vtkTestingRendering
